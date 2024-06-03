cfb5289974d4e55e95f5d540ee8556137a232d19
Hussein <husseinsym3@gmail.com>
2024-06-04 00:02:34 +0300

    edit chromium 6

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer");
+
 //const chromium = require("@sparticuz/chromium");
 
-const { CHROME_EXECUTABLE_PATH } = require("./src/config");
+//const { CHROME_EXECUTABLE_PATH } = require("./src/config");
 const main = require("./src/main");
 
 const app = express();
@@ -13,13 +14,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   (async () => {
-    const browser = await chromium.puppeteer.launch({
-      args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: true,
-      ignoreHTTPSErrors: true,
-    });
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
     main(browser);
   })();
   res.json({ hi: "Hi" });
