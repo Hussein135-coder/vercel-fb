93ab3763721029ea149a3eec781b79ef8377f662
Hussein <husseinsym3@gmail.com>
2024-06-04 12:33:17 +0300

    edit chromium 1١

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const puppeteer = require("puppeteer-core");
-const chromium = require("@sparticuz/chromium");
+const chromium = require("chrome-aws-lambda");
 
-//const chromium = require("@sparticuz/chromium");
-
-//const { CHROME_EXECUTABLE_PATH } = require("./src/config");
+const { CHROME_EXECUTABLE_PATH } = require("./src/config");
 const main = require("./src/main");
+
+// const browser = await puppeteer.launch( { args: ['--no-sandbox'] } );
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
 
-chromium.setHeadlessMode = true;
-chromium.setGraphicsMode = false;
-
 app.get("/", (req, res) => {
   (async () => {
-    const browser = await puppeteer.launch({
-      args: chromium.args,
+    const browser = await chromium.puppeteer.launch({
+      args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
       defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(),
-      headless: chromium.headless,
+      executablePath: await chromium.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
     });
     main(browser);
   })();
